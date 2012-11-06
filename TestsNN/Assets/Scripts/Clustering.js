55b121b26f161f333e91d47ad390311b8011af6a
Marco Buzzoni <buzzo.88@hotmail.it>
2012-11-06 15:26:36 +0100

    Finisced clustering problem example (very basic)

--- TestsNN/Assets/Scripts/Clustering.js
+++ TestsNN/Assets/Scripts/Clustering.js
@@ -2,122 +2,138 @@
 
 private var input : Array;
 private var inputVec : Array;
+private var w:Array;
+
 
 function Start () {
 	
 	input = new Array();
 	inputVec = new Array();
 	
-	
-	var gameObject:GameObject;
-	
-	// vettore 0
-	gameObject = new GameObject();
-	var lRenderer:LineRenderer = gameObject.AddComponent(LineRenderer);
-	lRenderer.SetVertexCount(2);
-	lRenderer.SetWidth(0.2, 0.2);
-	lRenderer.material.color = Color.green;
-	//lRenderer.SetColors( Color.green, Color.green );
-	lRenderer.SetPosition(0, Vector3.zero );
-	lRenderer.SetPosition(1, Vector3.left * 5 );
-	inputVec.Push(new Vector2(-1.0, 0.0));
-	
-	input.Push(gameObject);
-	
-	
-	// vettore 1
-	gameObject = new GameObject();
-	lRenderer = gameObject.AddComponent(LineRenderer);
-	lRenderer.SetVertexCount(2);
-	lRenderer.SetWidth(0.2, 0.2);
-	lRenderer.material.color = Color.green;
-	//lRenderer.SetColors( Color.green, Color.green );
-	lRenderer.SetPosition(0, Vector3.zero );
-	//var tmp:Vector3 = Vector3.Slerp(Vector3.left, Vector3.right, 0.5);
-	lRenderer.SetPosition(1, new Vector3(-1.0, 1.0, 0.0).normalized * 5 );
-	inputVec.Push(new Vector2(-1.0, 1.0).normalized);
-	
-	input.Push(gameObject);
-	
-	// vettore 2
-	gameObject = new GameObject();
-	lRenderer = gameObject.AddComponent(LineRenderer);
-	lRenderer.SetVertexCount(2);
-	lRenderer.SetWidth(0.2, 0.2);
-	lRenderer.material.color = Color.green;
-	//lRenderer.SetColors( Color.green, Color.green );
-	lRenderer.SetPosition(0, Vector3.zero );
-	//var tmp:Vector3 = Vector3.Slerp(Vector3.left, Vector3.right, 0.5);
-	lRenderer.SetPosition(1, new Vector3(-0.8, 1.5, 0.0).normalized * 5 );
-	inputVec.Push( new Vector2(-0.8, 1.5).normalized);
-	
-	input.Push(gameObject);
-	
-	// vettore 3
-	gameObject = new GameObject();
-	lRenderer = gameObject.AddComponent(LineRenderer);
-	lRenderer.SetVertexCount(2);
-	lRenderer.SetWidth(0.2, 0.2);
-	lRenderer.material.color = Color.green;
-	//lRenderer.SetColors( Color.green, Color.green );
-	lRenderer.SetPosition(0, Vector3.zero );
-	//var tmp:Vector3 = Vector3.Slerp(Vector3.left, Vector3.right, 0.5);
-	lRenderer.SetPosition(1, new Vector3(Mathf.Cos(0.25), Mathf.Sin(0.25), 0.0) * 5 );
-	inputVec.Push( new Vector2(Mathf.Cos(0.25), Mathf.Sin(0.25)) );
-	
-	// vettore 3
-	gameObject = new GameObject();
-	lRenderer = gameObject.AddComponent(LineRenderer);
-	lRenderer.SetVertexCount(2);
-	lRenderer.SetWidth(0.2, 0.2);
-	lRenderer.material.color = Color.green;
-	//lRenderer.SetColors( Color.green, Color.green );
-	lRenderer.SetPosition(0, Vector3.zero );
-	//var tmp:Vector3 = Vector3.Slerp(Vector3.left, Vector3.right, 0.5);
-	lRenderer.SetPosition(1, new Vector3(Mathf.Cos(0.12), Mathf.Sin(0.12), 0.0) * 5 );
-	inputVec.Push( new Vector2(Mathf.Cos(0.12), Mathf.Sin(0.12)) );
-	
-	input.Push(gameObject);
+	createVector(0.12);
+	createVector(0.32);
+	createVector(1.44);
+	createVector(2.89);
+	createVector(3.14);
 	
 	
 	/*
 	L'insieme di vettori di input X è inputVec. (pag 112 libro)
 	scelgo il numero di cluster k=2, e genero a caso 2 pesi.
 	*/
-	var w:Array = new Array();
+	w = new Array();
+	
 	var x = Random.value * Mathf.PI;
-	w.Push(new Vector2(Mathf.Sin(x), Mathf.Cos(x)));
-	Debug.Log(x);
+	var tmp:Vector2;
+	tmp = new Vector2(Mathf.Sin(x), Mathf.Cos(x));
+	tmp.Normalize();
+	w.Push( tmp );
+	
 	x = Random.value * Mathf.PI;
-	w.Push(new Vector2(Mathf.Sin(x), Mathf.Cos(x)));
-	Debug.Log(x);
+	tmp = new Vector2(Mathf.Sin(x), Mathf.Cos(x));
+	tmp.Normalize();
+	w.Push( tmp );
+	
+	x = Random.value * Mathf.PI;
+	tmp = new Vector2(Mathf.Sin(x), Mathf.Cos(x));
+	tmp.Normalize();
+	w.Push( tmp );
 	
 	
-	// select a vector xj randomly
-	var j:int = Mathf.Round(Random.value * 4);
-	Debug.Log(inputVec);
-	 
-	// calculate xj * vi for all i
-	var m:int = 0;
-	var mVal = 0;
-	var indice = 0;
-	var xj:Vector2 = Vector2.zero;
-	for (var mioJ:Vector2 in inputVec)	{
-		Debug.Log(indice + " -> " + mioJ);
-		if(indice == j)
-			xj = mioJ;
-		indice ++;
-	}
-	indice = 0;
-	for (var wi:Vector2 in w)	{
+	Debug.Log("Vettore dei pesi: " + w);
 	
-		var tmpResult = xj.x * wi.x + xj.y * wi.y;
-		if(tmpResult > mVal)	{
-			Debug.Log("m " + indice);
-			m=indice;
+	
+	for (var i:int = 0; i < 500; i++)	{
+		
+		// select a vector xj randomly
+		var j:int = Mathf.Round(Random.value * 4);
+		Debug.Log("Vettore degli input: " + inputVec);
+		 
+		// calculate xj * vi for all i
+		var m:int = 0;
+		var mVal = 0;
+		var indice = 0;
+		var xj:Vector2 = Vector2.zero;
+	
+		xj = inputVec[j];
+		indice = 0;
+		for (var wi:Vector2 in w)	{
+		
+			var tmpResult = xj.x * wi.x + xj.y * wi.y;
+			// alla fine m contiene l'indice del vettore w tale che w*xj è maggiore.
+			if(tmpResult > mVal)	{
+				mVal = tmpResult;
+				Debug.Log("m " + indice);
+				m=indice;
+			}
+			indice ++;
 		}
-		indice ++;
+		
+		Debug.Log("m vale " + m);
+		var tmp2:Vector2 = w[m];
+		var somma:Vector2 = tmp2 + xj;
+		somma.Normalize();
+		w[m] = somma;
+	
 	}
+	
+	Debug.Log("Vettore dei pesi FINALE " + w);
+	
+	for (var wcur:Vector2 in w)	{
+		createVector(wcur.x, wcur.y);
+	}/*
+	var aa:Vector2 = w[0];
+	var bb:Vector2 = w[1];
+	var cc:Vector2 = w[3];
+	createVector(aa.x, aa.y);
+	createVector(bb.x, bb.y);*/
+}
+
+function createVector(angle:float)	{
+	// vettore 3
+	var gameObject:GameObject = new GameObject();
+	var lRenderer = gameObject.AddComponent(LineRenderer);
+	lRenderer.SetVertexCount(2);
+	lRenderer.SetWidth(0.2, 0.2);
+	lRenderer.material.color = Color.green;
+	//lRenderer.SetColors( Color.green, Color.green );
+	lRenderer.SetPosition(0, Vector3.zero );
+	//var tmp:Vector3 = Vector3.Slerp(Vector3.left, Vector3.right, 0.5);
+	
+	var tmp:Vector2 = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+	lRenderer.SetPosition(1, new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0.0) * 5 );
+	
+	tmp.Normalize();
+	inputVec.Push( tmp );
+	
+	input.Push(gameObject);
+}
+
+function createVector(x:float, y:float)	{
+	// vettore 3
+	var gameObject:GameObject = new GameObject();
+	var lRenderer = gameObject.AddComponent(LineRenderer);
+	lRenderer.SetVertexCount(2);
+	lRenderer.SetWidth(0.2, 0.2);
+	lRenderer.material.color = Color.red;
+	//lRenderer.SetColors( Color.green, Color.green );
+	lRenderer.SetPosition(0, Vector3.zero );
+	//var tmp:Vector3 = Vector3.Slerp(Vector3.left, Vector3.right, 0.5);
+	
+	var tmp2:Vector3 = new Vector3(x, y, 0.0);
+	tmp2.Normalize();
+	lRenderer.SetPosition(1, tmp2 * 5 );
+	
+	
+	input.Push(gameObject);
+}
+
+function test()	{
+	
+}
+
+/* sostituisce wm con wm * xj */
+function update(m:int, xj:Vector2)	{
 	
 }
 
